e9e996756031295b315256c3b896e06e82e68f26
Haryanti Losiana <hlosiana@Haryantis-MacBook-Pro.local>
2019-10-13 11:22:55 +0700

    Second Update

--- js/main.js
+++ js/main.js
@@ -108,7 +108,7 @@ function getMovieDisp() {
             <div class="col-md-3">
               <div class="card-body">
                 <h5 class="card-title">${movieDis.original_title}</h5>
-                <p class="card-text">${movieDis.overview}</p>
+               
                 <p class="card-text">Released : ${movieDis.release_date}</p>
                 <a onclick="idMovie('${movieDis.id}')" class="btn btn-primary" href="#">Movie Description</a>
             </div>
@@ -141,7 +141,7 @@ function getMovieUpcoming() {
             <div class="col-md-3">
               <div class="card-body">
                 <h5 class="card-title">${movieDis.original_title}</h5>
-                <p class="card-text">${movieDis.overview}</p>
+                
                 <p class="card-text">Released : ${movieDis.release_date}</p>
                 <a onclick="idMovie('${movieDis.id}')" class="btn btn-primary" href="#">Movie Description</a>
             </div>
@@ -205,6 +205,212 @@ function getMovieDis() {
            movieId.imdb_id
          }" target="_blank" class="btn btn-primary">View IMDB</a>
          <a href='upcoming.html' class="btn btn-success">Back to Search </a>
+       </div>
+      </div>
+      
+      `;
+      $("#moviesDis").html(outputDesc);
+    });
+}
+
+function trendingMovie() {
+  axios
+    .get(
+      "https://api.themoviedb.org/3/trending/all/day?api_key=0ccd87f544c7e54dafba594584acc692"
+    )
+    .then(response => {
+      console.log(response);
+      let movieDisp = response.data.results;
+      let outputMovie = "";
+
+      $.each(movieDisp, (index, movieDis) => {
+        outputMovie += `
+            <div class="col-md-3">
+              <img src="https://image.tmdb.org/t/p/w500/${movieDis.poster_path}
+              " class="card-img mb-3" alt="...">
+            </div>
+
+            <div class="col-md-3">
+              <div class="card-body">
+                <h5 class="card-title">${movieDis.original_title}</h5>
+                
+                <p class="card-text">Released : ${movieDis.release_date}</p>
+                <a onclick="idMovie('${movieDis.id}')" class="btn btn-primary" href="#">Movie Description</a>
+            </div>
+                
+              </div>
+            </div>
+        `;
+      });
+      $("#movieDisplay").html(outputMovie);
+    });
+}
+
+function tvPop() {
+  axios
+    .get(
+      "https://api.themoviedb.org/3/tv/popular?api_key=0ccd87f544c7e54dafba594584acc692&language=en-US&page=1"
+    )
+    .then(response => {
+      console.log(response);
+      let movieDisp = response.data.results;
+      let outputMovie = "";
+
+      $.each(movieDisp, (index, movieDis) => {
+        outputMovie += `
+            <div class="col-md-3">
+              <img src="https://image.tmdb.org/t/p/w500/${movieDis.poster_path}
+              " class="card-img mb-3" alt="...">
+            </div>
+
+            <div class="col-md-3">
+              <div class="card-body">
+                <h5 class="card-title">${movieDis.original_name}</h5>
+                <p class="card-text">Popularity : ${movieDis.popularity}</p>
+                <a onclick="idMovie('${movieDis.id}')" class="btn btn-primary" href="#">Tv Show Description</a>
+            </div>
+                
+              </div>
+            </div>
+        `;
+      });
+      $("#movieDisplay").html(outputMovie);
+    });
+}
+
+function tvDesc() {
+  let movId = sessionStorage.getItem("movie_id");
+  axios
+    .get(
+      "https://api.themoviedb.org/3/tv/" +
+        movId +
+        "?api_key=0ccd87f544c7e54dafba594584acc692&language=en-US"
+    )
+    .then(response => {
+      console.log(response);
+      let movieId = response.data;
+      let outputDesc = `
+      <div class="row">
+      <div class="col-md-4">
+        <img src="https://image.tmdb.org/t/p/w300/${
+          movieId.poster_path
+        }" class="thumbnail">
+      </div>
+      <div class="col-md-8">
+      <h3 class="text-center">${movieId.original_name}</h3>
+       <ul class="list-group">
+       <li class="list-group-item"><strong>Genre :</strong> ${
+         movieId.genres[0]["name"]
+       }, ${movieId.genres[1]["name"]}</li>
+    
+       <li class="list-group-item"><strong>Created by :</strong> ${
+         movieId.created_by[0]["name"]
+       }</li>
+
+       <li class="list-group-item"><strong>Production :</strong> ${
+         movieId.production_companies[0]["name"]
+       }
+        </li>
+
+        
+      
+       </ul>
+      </div>
+      </div>
+ 
+      <div class="row mt-3">
+       <div class="well">
+         <h3>Plot</h3>
+         ${movieId.overview}
+         <hr>
+         <a href='tvSeasons.html' class="btn btn-success">Back to Search </a>
+       </div>
+      </div>
+      
+      `;
+      $("#moviesDis").html(outputDesc);
+    });
+}
+
+function tvEps() {
+  axios
+    .get(
+      "https://api.themoviedb.org/3/tv/airing_today?api_key=0ccd87f544c7e54dafba594584acc692&language=en-US&page=1"
+    )
+    .then(response => {
+      console.log(response);
+      let movieDisp = response.data.results;
+      let outputMovie = "";
+
+      $.each(movieDisp, (index, movieDis) => {
+        outputMovie += `
+            <div class="col-md-3">
+              <img src="https://image.tmdb.org/t/p/w500/${movieDis.poster_path}
+              " class="card-img mb-3" alt="...">
+            </div>
+
+            <div class="col-md-3">
+              <div class="card-body">
+                <h5 class="card-title">${movieDis.original_name}</h5>
+                <p class="card-text">${movieDis.popularity}</p>
+                <p class="card-text">Released : ${movieDis.first_air_date}</p>
+                <a onclick="idMovie('${movieDis.id}')" class="btn btn-primary" href="#">Tv Show Description</a>
+            </div>
+                
+              </div>
+            </div>
+        `;
+      });
+      $("#movieDisplay").html(outputMovie);
+    });
+}
+
+function tvAir() {
+  let movId = sessionStorage.getItem("movie_id");
+  axios
+    .get(
+      "https://api.themoviedb.org/3/tv/" +
+        movId +
+        "?api_key=0ccd87f544c7e54dafba594584acc692&language=en-US"
+    )
+    .then(response => {
+      console.log(response);
+      let movieId = response.data;
+      let outputDesc = `
+      <div class="row">
+      <div class="col-md-4">
+        <img src="https://image.tmdb.org/t/p/w500/${
+          movieId.poster_path
+        }" class="thumbnail">
+      </div>
+      <div class="col-md-8">
+      <h3 class="text-center">${movieId.results[0]["name"]}</h3>
+       <ul class="list-group">
+       <li class="list-group-item"><strong>Genre :</strong> ${
+         movieId.genres[0]["name"]
+       }</li>
+    
+       <li class="list-group-item"><strong>Created by :</strong> ${
+         movieId.created_by[0]["name"]
+       }</li>
+
+       <li class="list-group-item"><strong>Production :</strong> ${
+         movieId.production_companies[1]["name"]
+       }
+        </li>
+
+       
+      
+       </ul>
+      </div>
+      </div>
+ 
+      <div class="row mt-3">
+       <div class="well">
+         <h3>Plot</h3>
+         ${movieId.overview}
+         <hr>
+         <a href='tvEpisode.html' class="btn btn-success">Back to Search </a>
        </div>
       </div>
       
